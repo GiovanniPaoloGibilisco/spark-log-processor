23bb49db89ac230b9142468be2a1d958857f84fe
GiovanniPaoloGibilisco <gibbo87@gmail.com>
2015-05-01 16:22:44 -0400

    Removed compile plugin from pom, addedd collection and export of Task information into a CSV file for further analysis

--- sparkloggerparser/src/main/java/it/polimi/spark/LoggerParser.java
+++ sparkloggerparser/src/main/java/it/polimi/spark/LoggerParser.java
@@ -1,12 +1,21 @@
 package it.polimi.spark;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,62 +24,98 @@ public class LoggerParser {
 
 	static final Logger logger = LoggerFactory.getLogger(LoggerParser.class);
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException,
+			URISyntaxException {
 
+		// the configuration of the application (as launched by the user)
 		Config.init(args);
 		Config config = Config.getInstance();
 		if (!Files.exists(Paths.get(config.inputFile))) {
 			logger.error("Input file does not exist");
 			return;
 		}
-		SparkConf conf = new SparkConf().setAppName("logger-parser").setMaster("local[1]");
+
+		// the spark configuration
+		SparkConf conf = new SparkConf().setAppName("logger-parser").setMaster(
+				"local[1]");
 		JavaSparkContext sc = new JavaSparkContext(conf);
-		
-		
 		SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
 
-		DataFrame logsframe = sqlContext.jsonFile(config.inputFile);
-		logsframe.cache();		
-		logsframe.registerTempTable("events");
-	
-		
-		sqlContext.sql("SELECT * FROM events WHERE Event LIKE '%TaskStart'").registerTempTable("taskStartInfos");
-		sqlContext.sql("SELECT * FROM events WHERE Event LIKE '%TaskEnd'").registerTempTable("taskEndInfos");
-		
-		
-		DataFrame taskDetails = sqlContext.sql("SELECT 	`start.Task Info.Task ID` AS id,"
-				+ "				`start.Stage ID` AS stageID,"							
-				+ "				`start.Task Info.Executor ID` AS executorID,"
-				+ "				`start.Task Info.Host` AS host,"
-				+ "				`finish.Task Type` AS type,"
-				+ "				`finish.Task Info.Finish Time` - `start.Task Info.Launch Time` AS executionTime,"
-				+ "				`finish.Task Metrics.Executor Run Time` AS executorRunTime,"
-				+ "				`finish.Task Metrics.Executor Deserialize Time` AS executorDeserializerTime,"
-				+ "				`finish.Task Metrics.Result Serialization Time` AS resultSerializationTime,"
-				+ "				`finish.Task Metrics.Shuffle Write Metrics.Shuffle Write Time` AS shuffleWriteTime,"
-				+ "				`finish.Task Metrics.JVM GC Time` AS GCTime,"				
-				+ "				`finish.Task Metrics.Result Size` AS resultSize,"
-				+ "				`finish.Task Metrics.Memory Bytes Spilled` AS memoryBytesSpilled,"
-				+ "				`finish.Task Metrics.Disk Bytes Spilled` AS diskBytesSpilled,"
-				+ "				`finish.Task Metrics.Shuffle Write Metrics.Shuffle Bytes Written` AS shuffleBytesWritten,"
-				+ "				`finish.Task Metrics.Shuffle Write Metrics.Shuffle Records Written` AS shuffleRecordsWritten,"
-				+ "				`finish.Task Metrics.Input Metrics.Data Read Method` AS dataReadMethod,"
-				+ "				`finish.Task Metrics.Input Metrics.Bytes Read` AS bytesRead,"
-				+ "				`finish.Task Metrics.Input Metrics.Records Read` AS recordsRead,"
-				+ "				`finish.Task Metrics.Shuffle Read Metrics.Remote Blocks Fetched` AS shuffleRemoteBlocksFetched,"
-				+ "				`finish.Task Metrics.Shuffle Read Metrics.Local Blocks Fetched` AS shuffleLocalBlocksFetched,"
-				+ "				`finish.Task Metrics.Shuffle Read Metrics.Fetch Wait Time` AS shuffleFetchWaitTime,"
-				+ "				`finish.Task Metrics.Shuffle Read Metrics.Remote Bytes Read` AS shuffleRemoteBytesRead,"
-				+ "				`finish.Task Metrics.Shuffle Read Metrics.Local Bytes Read` AS shuffleLocalBytesRead,"
-				+ "				`finish.Task Metrics.Shuffle Read Metrics.Total Records Read` AS shuffleTotalRecordsRead"
-				+ "		FROM taskStartInfos AS start"
-				+ "		JOIN taskEndInfos AS finish"
-				+ "		ON `start.Task Info.Task ID`=`finish.Task Info.Task ID`");
-		
-		taskDetails.show();
-		
-		
+		// the hadoop configuration
+		Configuration hadoopConf = new Configuration();
+		FileSystem hdfs = FileSystem.get(hadoopConf);
+		if (hdfs.exists(new Path(config.outputFile)))
+			hdfs.delete(new Path(config.outputFile), true);
 
+		// load the logs
+		DataFrame logsframe = sqlContext.jsonFile(config.inputFile);
+		logsframe.cache();
+
+		// register the main table with all the logs as "events"
+		logsframe.registerTempTable("events");
+
+		// register two tables, one for the task start event and the other for
+		// the task end event
+		sqlContext.sql("SELECT * FROM events WHERE Event LIKE '%TaskStart'")
+				.registerTempTable("taskStartInfos");
+		sqlContext.sql("SELECT * FROM events WHERE Event LIKE '%TaskEnd'")
+				.registerTempTable("taskEndInfos");
+
+		// query the two tables for the task details
+		DataFrame taskDetails = sqlContext
+				.sql("SELECT 	`start.Task Info.Task ID` AS id,"
+						+ "				`start.Stage ID` AS stageID,"
+						+ "				`start.Task Info.Executor ID` AS executorID,"
+						+ "				`start.Task Info.Host` AS host,"
+						+ "				`finish.Task Type` AS type,"
+						+ "				`finish.Task Info.Finish Time` - `start.Task Info.Launch Time` AS executionTime,"
+						+ "				`finish.Task Metrics.Executor Run Time` AS executorRunTime,"
+						+ "				`finish.Task Metrics.Executor Deserialize Time` AS executorDeserializerTime,"
+						+ "				`finish.Task Metrics.Result Serialization Time` AS resultSerializationTime,"
+						+ "				`finish.Task Metrics.Shuffle Write Metrics.Shuffle Write Time` AS shuffleWriteTime,"
+						+ "				`finish.Task Metrics.JVM GC Time` AS GCTime,"
+						+ "				`finish.Task Metrics.Result Size` AS resultSize,"
+						+ "				`finish.Task Metrics.Memory Bytes Spilled` AS memoryBytesSpilled,"
+						+ "				`finish.Task Metrics.Disk Bytes Spilled` AS diskBytesSpilled,"
+						+ "				`finish.Task Metrics.Shuffle Write Metrics.Shuffle Bytes Written` AS shuffleBytesWritten,"
+						+ "				`finish.Task Metrics.Shuffle Write Metrics.Shuffle Records Written` AS shuffleRecordsWritten,"
+						+ "				`finish.Task Metrics.Input Metrics.Data Read Method` AS dataReadMethod,"
+						+ "				`finish.Task Metrics.Input Metrics.Bytes Read` AS bytesRead,"
+						+ "				`finish.Task Metrics.Input Metrics.Records Read` AS recordsRead,"
+						+ "				`finish.Task Metrics.Shuffle Read Metrics.Remote Blocks Fetched` AS shuffleRemoteBlocksFetched,"
+						+ "				`finish.Task Metrics.Shuffle Read Metrics.Local Blocks Fetched` AS shuffleLocalBlocksFetched,"
+						+ "				`finish.Task Metrics.Shuffle Read Metrics.Fetch Wait Time` AS shuffleFetchWaitTime,"
+						+ "				`finish.Task Metrics.Shuffle Read Metrics.Remote Bytes Read` AS shuffleRemoteBytesRead,"
+						+ "				`finish.Task Metrics.Shuffle Read Metrics.Local Bytes Read` AS shuffleLocalBytesRead,"
+						+ "				`finish.Task Metrics.Shuffle Read Metrics.Total Records Read` AS shuffleTotalRecordsRead"
+						+ "		FROM taskStartInfos AS start"
+						+ "		JOIN taskEndInfos AS finish"
+						+ "		ON `start.Task Info.Task ID`=`finish.Task Info.Task ID`");
+
+
+		// collect the results and write them to a file
+		List<Row> taskList = taskDetails.toJavaRDD().collect();
+		logger.info("Found "+taskList.size()+" tasks");
+		OutputStream os = hdfs.create(new Path(config.outputFile,
+				"TaskDetails.csv"));
+		BufferedWriter br = new BufferedWriter(new OutputStreamWriter(os,
+				"UTF-8"));
+		// the schema first
+		for (String column : taskDetails.columns())
+			br.write(column + ",");
+		br.write("\n");
+		// the values later
+		for (Row task : taskList) {
+			for (int i = 0; i < task.size(); i++) 
+				br.write(task.get(i) + ",");			
+			br.write("\n");
+		}
+		br.close();
+		
+		
+		
+		
+		hdfs.close();
 	}
 
 }
